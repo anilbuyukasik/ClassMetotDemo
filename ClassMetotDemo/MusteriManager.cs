1d8ce42d2718028c582ad3200dddf2a81bf285bd
Anıl Büyükaşık <buyukasik.anil@gmail.com>
2021-01-29 20:28:14 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " nolu musteri eklendi");
+        }
+        public void Guncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " nolu musteri guncellendi");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " nolu musteri silindi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri { Id = 1, Ad = "Anıl", Soyad = "Nüyükaşık", DogumYeri = "Hatay" };
+            Musteri musteri2 = new Musteri { Id = 2, Ad = "Nurnilay", Soyad = "Pınar", DogumYeri = "Trabzon" };
+            Musteri musteri3 = new Musteri { Id = 3, Ad = "Sansa", Soyad = "Büyükaşık", DogumYeri = "Hatay" };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Guncelle(musteri2);
+            musteriManager.Sil(musteri3);
+        }
+    }
+}
